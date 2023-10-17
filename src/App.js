682592d76a282a1fbc67c72c303a5a34730ca356
eklownr <eklownr@gmail.com>
2023-10-17 15:21:40 +0200

    add topNav

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import TopNav from './components/header/topNav';
 function App() {
   return (
     <div className="App">
-      <home/>
+      <TopNav/>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<TopNav/>}></Route>
+          <Route path='/' element={<Home/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path='/about' element={<About/>}></Route>
           <Route path='*' element={<NotFound/>}></Route>
