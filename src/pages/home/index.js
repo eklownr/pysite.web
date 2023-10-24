5ba0387238acb42b1e7e8520f6e5e2f041304211
eklownr <eklownr@gmail.com>
2023-10-24 14:44:33 +0200

    add useState, useEffect on /home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,22 @@
 import '../../styles/App.css'
+import React, {useState, useEffect} from 'react';
+
 
 const Home = () => {
     // javascrift
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch("/api/items")
+        .then(response => response.json())
+        .then(data => setData(data))
+    }, []);
+
     return (
         // html kod
         <div className='bg-color'>  
             <h1>Home page!</h1>
+            <p>{data ? data.user_name : 'loading... '}</p>
         </div>
     )
 };
