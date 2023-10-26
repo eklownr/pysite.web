f02c347f7595c78cffcf127bee15527550b0bc63
eklownr <eklownr@gmail.com>
2023-10-26 20:46:51 +0200

    add list to HOME

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,14 @@
 import '../../styles/App.css'
 import React, {useState, useEffect} from 'react';
 
+const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
 
 const Home = () => {
     // javascrift
     const [data, setData] = useState(null)
 
     useEffect(() => {
-        fetch("/api/items")
+        fetch("/api/items", {Method: "GET"})
         .then(response => response.json())
         .then(data => setData(data))
     }, []);
@@ -16,14 +17,30 @@ const Home = () => {
         // html kod
         <div>  
             <h1>Home page!</h1>
-            <div className='block'>
-                <p>Data from backend:</p>
-                Name: {data ? data.user_name : 'loading...'}<br/>
-                Age:  {data ? data.age : 'Loading..'}<br/>
-                Id:   {data ? data.id : 'Loading..'}
+            <div class="row">
+                <div class="column">
+                    <div className='block'>
+                        <p>Data from backend:</p>
+                        Name: {data ? data.user_name : 'loading...'}<br/>
+                        Age:  {data ? data.age : 'Loading..'}<br/>
+                        Id:   {data ? data.id : 'Loading..'}
+                    </div>
+                    <br/>
+                    <div className='block'>
+                        <p>Names from  a list using .map</p>
+                        {names.map(name => (<li>{name}</li>))}
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column">
+                    <div className='block'>
+                        <p>Names from  a list using .map</p>
+                        {names.map(name => (<li>{name}</li>))}
+                    </div>
+                </div>
             </div>
         </div>
     )
 };
-
 export default Home;
