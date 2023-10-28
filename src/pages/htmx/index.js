a87c55e64e15caecad2b3cd92f58fa70bf77e77d
eklownr <eklownr@gmail.com>
2023-10-28 22:48:54 +0200

    try to get htmx to work

--- src/pages/htmx/index.js
+++ src/pages/htmx/index.js
@@ -6,12 +6,20 @@ const Htmx = () => {
         // html kod
         <div>
             <h2>HTMX</h2>
+            <div className='block'>
+                <p>add htmx-test here...</p>
+                <script src="https://unpkg.com/htmx.org@1.9.6" integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni" crossorigin="anonymous"></script>
 
-            <p>add htmx-test here...</p>
-            <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-            <button hx-post="/clicked" hx-swap="outerHTML">
-            Click Me
-            </button>
+                <button 
+                hx-post="/info" 
+                hx-select="#swaped"
+                hx-swap="outerHTML">
+                Click Me
+                </button>
+                <div id='swaped'>
+                    <p>some text....</p>
+                </div>
+            </div>
         </div>
     )
 };
