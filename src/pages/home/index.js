a26f5dfd1aa6b4f4836043d42406742f172b2120
eklownr <eklownr@gmail.com>
2023-10-28 17:50:52 +0200

    add potters.map

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import '../../styles/App.css'
 import React, {useState, useEffect} from 'react';
 
-const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
+//const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
 
 const Home = () => {
     // javascrift
@@ -13,30 +13,39 @@ const Home = () => {
 //        .then(data => setData(data))
 //    }, []);
 //
-    const [data, setData] = useState(null)
+    const [potters, setPotter] = useState(null)
 
     useEffect(() => {
         fetch("/api/potter", {Method: "GET"})
         .then(response => response.json())
-        .then(data => setData(data))
+        .then(data => setPotter(data))
+        .catch((error) => console.error(error));
     }, []);
 
     return (
         // html kod
+        // 
+//      {potters.map((person) => (
+//              <li key={person.id}>{person.id}{person.name}</li>
+//      ))}
         <div>  
             <h1>Home page!</h1>
             <div class="row">
                 <div class="column">
                     <div className="block">
                         <p>Data from backend:</p>
-                        ID: {data ? data[0].id : 'loading...'}<br/>
-                        Name:  {data ? data[0].name : 'Loading..'}<br/>
+                        ID: {potters ? potters[0].id : 'loading...'}<br/>
+                        Name:  {potters ? potters[0].name : 'Loading..'}<br/>
                     </div>
                 </div>
                 <did class="column">
                     <div className="block">
-                        <p>Names from  a list using .map</p>
-                        {names.map(name => (<li>{name}</li>))}
+                        <p>Names from a list using .map</p>
+                        <ul>
+                            {potters.map((person) => (
+                                <li key={person.id}>{person.id}{person.name}</li>
+                            ))}
+                        </ul>
                     </div>
                 </did>
             </div>
