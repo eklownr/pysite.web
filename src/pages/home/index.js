e3a2abc3f0baf256c190aa4c51cfa42793cfdec5
eklownr <eklownr@gmail.com>
2023-10-28 15:42:13 +0200

    Data from backend

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,10 +5,18 @@ const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
 
 const Home = () => {
     // javascrift
+//    const [data, setData] = useState(null)
+//
+//    useEffect(() => {
+//        fetch("/api/items", {Method: "GET"})
+//        .then(response => response.json())
+//        .then(data => setData(data))
+//    }, []);
+//
     const [data, setData] = useState(null)
 
     useEffect(() => {
-        fetch("/api/items", {Method: "GET"})
+        fetch("/api/potter", {Method: "GET"})
         .then(response => response.json())
         .then(data => setData(data))
     }, []);
@@ -19,26 +27,18 @@ const Home = () => {
             <h1>Home page!</h1>
             <div class="row">
                 <div class="column">
-                    <div className='block'>
+                    <div className="block">
                         <p>Data from backend:</p>
-                        Name: {data ? data.user_name : 'loading...'}<br/>
-                        Age:  {data ? data.age : 'Loading..'}<br/>
-                        Id:   {data ? data.id : 'Loading..'}
+                        ID: {data ? data[0].id : 'loading...'}<br/>
+                        Name:  {data ? data[0].name : 'Loading..'}<br/>
                     </div>
-                    <br/>
-                    <div className='block'>
+                </div>
+                <did class="column">
+                    <div className="block">
                         <p>Names from  a list using .map</p>
                         {names.map(name => (<li>{name}</li>))}
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="column">
-                    <div className='block'>
-                        <p>Names from  a list using .map</p>
-                        {names.map(name => (<li>{name}</li>))}
-                    </div>
-                </div>
+                </did>
             </div>
         </div>
     )
