725a77d4a6901c445cf7306d2e38cf527850729e
eklownr <eklownr@gmail.com>
2023-10-30 09:42:05 +0100

    htmx, Invalid DOM property

--- src/pages/htmx/index.js
+++ src/pages/htmx/index.js
@@ -9,16 +9,15 @@ const Htmx = () => {
             <div className='block'>
                 <p>add htmx-test here...</p>
                 <script src="https://unpkg.com/htmx.org@1.9.6" integrity="sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni" crossorigin="anonymous"></script>
-
-                <button 
-                hx-post="/info" 
-                hx-select="#swaped"
-                hx-swap="outerHTML">
-                Click Me
-                </button>
-                <div id='swaped'>
-                    <p>some text....</p>
-                </div>
+             
+                <div className='block'>
+                    <button
+                    hx-get="https://v2.jokeapi.dev/joke/Any?format=txt&safe-mode"
+                    hx-target="#output2"
+                    >Amuse Me</button>
+                    <p id="output2">Jokes from jokeapi.dev</p>
+                </div> 
+             
             </div>
         </div>
     )
